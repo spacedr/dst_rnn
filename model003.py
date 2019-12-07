e38e6dfcda24ede49e45f9355fe863574afc0fd0
spacedr <peter@lund.irf.se>
2019-12-07 17:30:06 +0100

    Reduced epochs to 500. Several bug fixes.

--- model003.py
+++ model003.py
@@ -9,7 +9,8 @@ import tensorflow as tf
 
 if __name__ == '__main__':
 
-    filename = 'model003.hdf5'
+    model_filename = 'model003.hdf5'
+    output_filename = 'model003.csv'
 
     inputs = ['bz', 'n', 'v']
     target = ['dst']
@@ -31,12 +32,12 @@ if __name__ == '__main__':
 
     res = model.fit(x_train, y_train,
                     validation_data=(x_val, y_val),
-                    callbacks=[tf.keras.callbacks.ModelCheckpoint(filename, save_best_only=True)],
+                    callbacks=[tf.keras.callbacks.ModelCheckpoint(model_filename, save_best_only=True)],
                     batch_size=1000,
-                    epochs=1000)
+                    epochs=500)
     pd.DataFrame(res.history).plot(logy=True)
 
-    model_best = tf.keras.models.load_model(filename)
+    model_best = tf.keras.models.load_model(model_filename)
 
     x = create_input(data, inputs, scaler_input, tau)
 
@@ -47,12 +48,13 @@ if __name__ == '__main__':
     z_best = pd.concat([data['dst'],
                         pd.Series(scaler_target.inverse_transform(model_best.predict(x)).ravel(),
                                   index=data.index, name='dst_pred')], axis=1)
+    z_best.to_csv(output_filename)
 
     print('Final model:')
-    print(compute_stats_per_year(z))
+    print(compute_stats_per_year(z['dst'], z['dst_pred']))
 
     print('Best model:')
-    print(compute_stats_per_year(z_best))
+    print(compute_stats_per_year(z_best['dst'], z_best['dst_pred']))
 
     z.plot(title='Prediction using final model')
 
